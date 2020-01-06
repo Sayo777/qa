b395b47a6eca5420406228faabfb72c3aedc6c0a
Sayo777 <49944996+Sayo777@users.noreply.github.com>
2020-01-06 11:07:12 +0800

    图片上传

--- src/main/java/com/sayo/qa/controller/InspectorController.java
+++ src/main/java/com/sayo/qa/controller/InspectorController.java
@@ -2,11 +2,14 @@ package com.sayo.qa.controller;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
+import com.qiniu.util.Auth;
+import com.qiniu.util.StringMap;
 import com.sayo.qa.CommonUtil.CommunityUtil;
 import com.sayo.qa.CommonUtil.DateUtil;
 import com.sayo.qa.entity.*;
 import com.sayo.qa.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -245,6 +248,50 @@ public class InspectorController {
         }else{
             return CommunityUtil.getJSONString(1,map.get("inspector2Msg").toString());
         }
+    }
+
+    @RequestMapping(path = "/myWizard",method = RequestMethod.GET)
+    public String getMyWizard(){
+        return "/hh/inspector/myWizard.html";
+    }
+
+
+
+    //文件处理上传
+    @Value("${qiniu.key.access}")
+    private String accessKey;
+
+    @Value("${qiniu.key.secret}")
+    private String secretKey;
+
+    @Value("${qiniu.bucket.file.name}")
+    private String headerBucketName;
+
+    @Value("${quniu.bucket.file.url}")
+    private String headerBucketUrl;
+
+    @RequestMapping(path = "/myUpload",method = RequestMethod.GET)
+    public String getUpload(Model model){
+        //生成长传文件的名称
+        String fileName = CommunityUtil.generateUUID();
+//        String fileName2 = CommunityUtil.generateUUID();
+//        String fileName3 = CommunityUtil.generateUUID();
+        // 设置响应信息
+        StringMap policy = new StringMap();
+        policy.put("returnBody", CommunityUtil.getJSONString(0));
+        // 生成上传凭证
+        Auth auth = Auth.create(accessKey, secretKey);
+        String uploadToken = auth.uploadToken(headerBucketName, fileName, 3600, policy);
+//        String uploadToken2 = auth.uploadToken(headerBucketName, fileName2, 3600, policy);
+//        String uploadToken3 = auth.uploadToken(headerBucketName, fileName3, 3600, policy);
+
+        model.addAttribute("uploadToken", uploadToken);
+        model.addAttribute("fileName", fileName);
+//        model.addAttribute("uploadToken2", uploadToken2);
+//        model.addAttribute("fileName2", fileName2);
+//        model.addAttribute("uploadToken3", uploadToken3);
+//        model.addAttribute("fileName3", fileName3);
+        return "/hh/inspector/webuploader.html";
     }
 
 
---
+++ src/test/java/com/sayo/qa/Demo.java
@@ -0,0 +1,5 @@
+package com.sayo.qa;
+
+class Demo {
+
+}
